5cd9586684b44949519f938e0c7b3ee06f62955b
David 'vSanjo' Woodward <hello@davidwoodward.me>
2021-01-17 12:17:56 +0800

    Removed content template page for initial deployment.

--- next.config.js
+++ next.config.js
@@ -5,5 +5,15 @@ module.exports = (phase, { defaultConfig }) => {
     env: {
       menu: menu.get('_pages'),
     },
+    webpack: (config, { isServer }) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty',
+        };
+      }
+
+      return config;
+    },
   };
 };
