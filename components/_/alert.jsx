801236a270404c2aa0d0fdf84ac415c3687916dc
FletcherAU <fletcher.boyd@artifactory.org.au>
2021-04-23 16:02:16 +0800

    COVID banner update

--- components/_/alert.jsx
+++ components/_/alert.jsx
@@ -3,7 +3,7 @@ export default function Alert() {
     // Container
     <div className="px-16 bg-accent-1 border-primary-2 bg-highlight-1 text-white font-medium">
       {/* Content */}
-      <div className="p-2 text-center text-sm">Welcome to the redesigned Artifactory website!</div>
+      <div className="p-2 text-center text-sm">The workshop is currently closed due to the region wide lockdown. We expect to reopen on Tuesday (27/04)</div>
     </div>
   );
 }
