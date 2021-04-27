648700f9f7c287cce96ef3a5d6d9197fb4750752
tazard <16077190+tazard@users.noreply.github.com>
2021-04-27 10:16:11 +0800

    Changed instead to message before covid message
    Chickened out putting a covid message. Also, "now open" could be misinterpreted as literally open with a keyholder there.

--- components/_/alert.jsx
+++ components/_/alert.jsx
@@ -3,7 +3,7 @@ export default function Alert() {
     // Container
     <div className="px-16 bg-accent-1 border-primary-2 bg-highlight-1 text-white font-medium">
       {/* Content */}
-      <div className="p-2 text-center text-sm">The Artifactory is open! Please observe COVID-19 restrictions.</div>
+      <div className="p-2 text-center text-sm">Welcome to the Artifactory website!</div>
     </div>
   );
 }
