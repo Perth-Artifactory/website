fe0a13f8c70676c1543ac1895b621c2938e4049b
FletcherAU <fletcher.boyd@artifactory.org.au>
2021-05-23 20:18:13 +0800

    Calendar https
    The iframe for the calendar needs to be served over tls

--- components/_/events-widget.jsx
+++ components/_/events-widget.jsx
@@ -7,7 +7,7 @@ export default function EventsWidget() {
       <div className="block bg-primary-1 p-6 text-white mb-2 sm:mb-20">
         <h3 className="text-center text-2xl sm:text-3xl mb-4 sm:mb-10 sm:mt-4">Events</h3>
         <iframe
-          src="http://space.artifactory.org.au/calendar/calendar.html"
+          src="https://space.artifactory.org.au/calendar/calendar.html"
           width="100%"
           height="600"
           frameBorder="0"></iframe>
