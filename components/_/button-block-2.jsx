479601d7ff8261c41b4e07acda4c20537727c6c9
spondy <34975940+spondy@users.noreply.github.com>
2021-05-24 00:46:25 +0800

    Update button-block-2.jsx

--- components/_/button-block-2.jsx
+++ components/_/button-block-2.jsx
@@ -11,16 +11,16 @@ export default function ButtonBlock1() {
             {/* Button */}
             <a
               className="block p-2 bg-secondary-1 hover:bg-secondary-2 focus:bg-secondary-2 text-highlight-2 font-semibold"
-              href="mailto:info@artifactory.org.au">
-              Book a Visit
+              href="https://space.artifactory.org.au/">
+              Check the Webcams
             </a>
           </li>
           <li className="block max-w-xs mb-2 mx-auto text-center w-100 sm:flex-grow sm:inline-block sm:mb-0 sm:mr-0 sm:w-1/2">
             {/* Outbound links will need the Next.js Link component */}
             <a
               className="block p-2 bg-secondary-1 hover:bg-secondary-2 focus:bg-secondary-2 text-highlight-2 font-semibold"
-              href="/pages/membership">
-              Become a Member
+              href="https://wiki.artifactory.org.au/">
+              Visit the Wiki
             </a>
           </li>
         </ul>
