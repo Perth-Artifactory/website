18d5396991ddef7dfe8f820746c5b9a3ce6da2cc
spondy <34975940+spondy@users.noreply.github.com>
2021-05-23 12:53:28 +0800

    Button text colour
    Button text changed to white

--- components/_/button-block-1.jsx
+++ components/_/button-block-1.jsx
@@ -10,7 +10,7 @@ export default function ButtonBlock1({ groupedBelow }) {
           <li className="block max-w-xs mb-2 mx-auto text-center w-100 sm:flex-grow sm:inline-block sm:mb-0 sm:mr-2 sm:w-1/2">
             {/* Button */}
             <a
-              className="block p-2 bg-secondary-1 hover:bg-secondary-2 focus:bg-secondary-2 text-primary-2 font-semibold"
+              className="block p-2 bg-secondary-1 hover:bg-secondary-2 focus:bg-secondary-2 text-highlight-2 font-semibold"
               href="mailto:info@artifactory.org.au">
               Book a Visit
             </a>
@@ -18,7 +18,7 @@ export default function ButtonBlock1({ groupedBelow }) {
           <li className="block max-w-xs mb-2 mx-auto text-center w-100 sm:flex-grow sm:inline-block sm:mb-0 sm:mr-0 sm:w-1/2">
             {/* Outbound links will need the Next.js Link component */}
             <a
-              className="block p-2 bg-secondary-1 hover:bg-secondary-2 focus:bg-secondary-2 text-primary-2 font-semibold"
+              className="block p-2 bg-secondary-1 hover:bg-secondary-2 focus:bg-secondary-2 text-highlight-2 font-semibold"
               href="/pages/membership">
               Become a Member
             </a>
