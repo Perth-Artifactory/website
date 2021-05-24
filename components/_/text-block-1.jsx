0fd4dd5072e467945daa46cf6b867fe0a6a9ea4e
spondy <34975940+spondy@users.noreply.github.com>
2021-05-24 23:08:15 +0800

    typos

--- components/_/text-block-1.jsx
+++ components/_/text-block-1.jsx
@@ -8,7 +8,7 @@ export default function TextBlock1() {
           <strong className="text-highlight-1">The Perth Artifactory</strong> is the Hackerspace/Makerspace in Perth, Western Australia. Located in Osborne Park, the Space is populated with tools, technology, materials, and workspaces, giving you the freedom and inspiration to create and bring your ideas to life. We are dedicated to providing a welcoming and safe space for people of all backgrounds and skill levels to come together to work on projects, share ideas, and socialise.
           <br />
           <br />
-          We are a not-for-profit organisation run by entirely volunteers (members) and currently have nearly 100 financial members and hundreds of regular visitors for our workshops and events.
+          We are a not-for-profit organisation run by entirely volunteers and currently have nearly 100 financial members and hundreds of regular visitors for our workshops and events.
         </p>
       </div>
     </>
