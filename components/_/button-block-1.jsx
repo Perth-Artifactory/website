27f3d0653b9b7ee4328216a6bcc873fbbe4f3ec7
spondy <34975940+spondy@users.noreply.github.com>
2021-05-24 16:01:34 +0800

    Update button-block-1.jsx

--- components/_/button-block-1.jsx
+++ components/_/button-block-1.jsx
@@ -2,7 +2,8 @@ export default function ButtonBlock1({ groupedBelow }) {
   return (
     <>
       {/* Container */}
-      <div className={`mt-5 sm:mt-10 mb-5 sm:mb-10 md:max-w-xl mx-auto ${groupedBelow ? 'mb-5' : 'mb-2' ? 'sm:mb-10' : 'sm:mb-2'} `}>
+      <div className={`mt-5 sm:mt-10 md:max-w-xl mx-auto ${groupedBelow ? 'mb-2' : 'sm:mb-5'}`}>
+      {/*  <div className={`mt-5 sm:mt-10 mb-5 sm:mb-10 md:max-w-xl mx-auto ${groupedBelow ? 'mb-2' : 'mb-5' ? 'sm:mb-10' : 'sm:mb-2'} `}>  */}
         {/* Button List */}
         <ul className="sm:flex justify-center">
           {/* Button List Item */}
