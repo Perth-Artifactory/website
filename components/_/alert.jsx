7cd4dd84516c22b17aa76febe0035dad2868e851
FletcherAU <me@fboyd.me>
2021-07-03 08:21:11 +0800

    Revert "Update banner for COVID lockdown"
    This reverts commit 37b92a1b478504b52eece41974f67af20dc43144.

--- components/_/alert.jsx
+++ components/_/alert.jsx
@@ -3,7 +3,7 @@ export default function Alert() {
     // Container
     <div className="px-16 bg-accent-1 border-primary-2 bg-highlight-1 text-white font-medium">
       {/* Content */}
-      <div className="p-2 text-center text-sm">Perth Artifactory temporarily closed due to COVID lockdown</div>
+      <div className="p-2 text-center text-sm">Welcome to the Artifactory website!</div>
     </div>
   );
 }
