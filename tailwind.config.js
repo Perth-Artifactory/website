b6f2936db85efe7cb3b5392aaa440d35f7b71d13
spondy <34975940+spondy@users.noreply.github.com>
2022-10-30 21:54:55 +0800

    link decoration

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,7 @@ module.exports = {
               backgroundColor: '#141414', // #26203b
             },
             a: {
+              text-decoration-line: 'none',
               color: '#bf1b00', // highlight-1
               hover: {
                 color: '#e32000', // secondary-2
