e8ffcb1d9a47235998169841724d3b57689a14a5
spondy <34975940+spondy@users.noreply.github.com>
2022-10-31 01:51:48 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,9 +72,9 @@ module.exports = {
             },
             a: {
               textDecoration: 'no-underline',
-              color: '#0000ff', // bf1b00 secondary-1
-              hover: {
-                color: '#00ff00', // ff2400 secondary-3
+              //color: '#bf1b00', // bf1b00 secondary-1
+              //hover: {
+              //  color: '#00ff00', // ff2400 secondary-3
               },
             },
           },
