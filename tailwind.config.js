b8026a3c6eda6fc561e33569a3579af0868b54c3
spondy <34975940+spondy@users.noreply.github.com>
2022-10-31 01:21:50 +0800

    retry

--- tailwind.config.js
+++ tailwind.config.js
@@ -76,7 +76,7 @@ module.exports = {
               //color: '#bf1b00', // secondary-1
               //hover: {
               //  color: '#ff2400', // secondary-3
-              },
+              //},
             },
           },
         },
