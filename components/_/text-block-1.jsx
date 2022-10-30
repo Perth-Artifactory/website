cf28898ae54d1ef367581565d5b1fed63b1e8239
spondy <34975940+spondy@users.noreply.github.com>
2022-10-31 00:26:50 +0800

    prices fix

--- components/_/text-block-1.jsx
+++ components/_/text-block-1.jsx
@@ -5,7 +5,7 @@ export default function TextBlock1() {
       <div className="block bg-primary-1 text-maintext p-6">
         {/* Content */}
         <p align="justify" className="">
-          <strong className="text-secondary-1">The Perth Artifactory</strong> is the Hackerspace/Makerspace in Perth, Western Australia. Located in Osborne Park, the Space is populated with tools, technology, materials, and workspaces, giving you the freedom and inspiration to create and bring your ideas to life. We are dedicated to providing a welcoming and safe space for people of all backgrounds and skill levels to come together to work on projects, share ideas, and socialise.
+          <strong>The Perth Artifactory</strong> is the Hackerspace/Makerspace in Perth, Western Australia. Located in Osborne Park, the Space is populated with tools, technology, materials, and workspaces, giving you the freedom and inspiration to create and bring your ideas to life. We are dedicated to providing a welcoming and safe space for people of all backgrounds and skill levels to come together to work on projects, share ideas, and socialise.
           <br />
           <br />
           We are a not-for-profit organisation run by entirely volunteers and currently host hundreds of members and visitors at our workshops and events.
