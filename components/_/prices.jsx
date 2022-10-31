8ed9b8d47e1fe455d19a050eac58487fda6f6760
spondy <34975940+spondy@users.noreply.github.com>
2022-11-01 00:32:21 +0800

    Update prices.jsx

--- components/_/prices.jsx
+++ components/_/prices.jsx
@@ -11,7 +11,7 @@ export default function Prices() {
       <div className="block p-2 bg-secondary-1 mb-5 sm:mb-10">
         <ul className="sm:flex sm:flex-wrap sm:space-between">
           <li className="block bg-primary-1 p-6 text-maintext mb-3 sm:mb-0 max-w-xs mx-auto sm:w-1/4 border-2 border-secondary-1 hover:border-4 hover:border-highlight-1">
-            <a className="block p-6" href="page.html">
+            <a className="block" href="page.html">
               <div>
                 <div className="text-4xl sm:text-xl text-highlight-1">
                   <ul className="block w-auto text-center my-4">
