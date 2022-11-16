ef2f9fc594724393b39162dbaf745dca30d7423d
spondy <34975940+spondy@users.noreply.github.com>
2022-11-16 10:55:28 +0800

    Added first visit page to sitemap

--- components/_/footer.jsx
+++ components/_/footer.jsx
@@ -14,6 +14,7 @@ export default function Footer() {
           <ul>
             <li><Link href="/"><a className="text-maintext">Home</a></Link></li>
             <li><Link href="https://shop.artifactory.org.au/"><a className="text-maintext">Store</a></Link></li>
+            <li><Link href="/pages/first_visit"><a className="text-maintext">First Visit</a></Link></li>
             <li><Link href="/pages/freeMembership"><a className="text-maintext">Free Membership</a></Link></li>
             <li><Link href="/pages/membership"><a className="text-maintext">Membership</a></Link></li>
             <li><Link href="https://wiki.artifactory.org.au/"><a className="text-maintext">Wiki</a></Link></li>
