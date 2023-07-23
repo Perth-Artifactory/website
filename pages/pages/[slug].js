9cad8b890971aff5e2c8b294571a46c909667e22
Fletcher <me@fboyd.me>
2023-07-23 18:25:52 +0800

    description test

--- pages/pages/[slug].js
+++ pages/pages/[slug].js
@@ -30,7 +30,7 @@ export default function Post({ post, morePosts, preview }) {
           {/* Content */}
           <div className="prose prose-white max-w-none" dangerouslySetInnerHTML={{ __html: post.content }}></div>
 
-          <div>Written by: {post.author}</div>
+          <div>Meta description bs: {post.description}</div>
 
         </div>
 
