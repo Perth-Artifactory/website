e4b2f00e0e162bb39ba9277e617122c4a9c48d16
Fletcher <fletcher.boyd@artifactory.org.au>
2023-07-24 15:47:50 +0800

    Add better plausible tracking (#45)
    * Add outbound link tracking
    
    * Add plausible events to homepage
    
    * Enable support for tagged events

--- components/_/contact.jsx
+++ components/_/contact.jsx
@@ -12,49 +12,49 @@ export default function Contact() {
           {/* Contact Item List Item */}
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Slack+TButton"
               href="https://perart.io/slack">
               <FaSlack />
             </a>
           </li>
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Facebook+TButton"
               href="https://www.facebook.com/pages/The-Perth-Artifactory/178514525501368">
               <FaFacebook />
             </a>
           </li>
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Twitter+TButton"
               href="https://twitter.com/perthartifact">
               <FaTwitter />
             </a>
           </li>
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Instagram+TButton"
               href="https://www.instagram.com/theperthartifactory/">
               <FaInstagram />
             </a>
           </li>
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Youtube+TButton"
               href="https://www.youtube.com/channel/UC9N4yG2QzYZV9Naud_IsUfA">
               <FaYoutube />
             </a>
           </li>
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Meetup+TButton"
               href="https://www.meetup.com/Perth-Artifactory/">
               <FaMeetup />
             </a>
           </li>
           <li className="inline-block">
             <a
-              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl"
+              className="block text-highlight-1 fill-current hover:text-secondary-1 text-xl sm:text-2xl md:text-3xl plausible-event-name=Github+TButton"
               href="https://github.com/Perth-Artifactory">
               <FaGithub />
             </a>
